391ef635156229359b15ec46bf71d38bf2ac1d6d
Arjxn13 <keygoldenwolf@gmail.com>
2025-02-18 12:18:57 -0700

    Update feb_18.cs

--- feb_18.cs
+++ feb_18.cs
@@ -1,1 +1,9 @@
-I learned how to declare a variable and use variables in my strings of code
+// I learned how to declare a variable and use variables in my strings of code
+
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
