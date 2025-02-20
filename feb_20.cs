dbb0c0592db22412b18280e33521bfadea63b587
Arjxn13 <keygoldenwolf@gmail.com>
2025-02-20 10:16:24 -0700

    Create feb_20.cs

---
+++ feb_20.cs
@@ -0,0 +1,11 @@
+// Today I learned how to asign values to a variable.
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
