200656fd30691d92810c4a50bd5d22a69948d515
Arjxn13 <keygoldenwolf@gmail.com>
2025-02-21 08:48:32 -0700

    Create feb_21.cs

---
+++ feb_21.cs
@@ -0,0 +1,6 @@
+// I learned how to use the variables I inserted to make a string of text.
+
+string firstName = "Arjun";
+int number = 13;
+double temp = 23.5;
+Console.WriteLine($"Hello. {firstName}! You have {number} messages in your inbox. The temperature is {temp} celcius.");
