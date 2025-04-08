5519000bcc9d6837a710497c3d6ae52e9b1a02a7
Arjxn13 <keygoldenwolf@gmail.com>
2025-04-08 12:13:18 -0600

    Create apr_8.cs

---
+++ apr_8.cs
@@ -0,0 +1,10 @@
+// Today I was given a challenge to make my code output a given response. I had 2 lines of code that I had to use, and may not change these two lines of code, but was allowed to
+// add code above and below each line to form the desired output.
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
