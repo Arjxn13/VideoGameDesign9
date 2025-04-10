93884216933530b0a88d167e0272f265c6590663
Arjxn13 <keygoldenwolf@gmail.com>
2025-04-10 10:07:31 -0600

    Create apr_10.cs

---
+++ apr_10.cs
@@ -0,0 +1,5 @@
+// Today I learned how to perform addition with implicit data conversion
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
